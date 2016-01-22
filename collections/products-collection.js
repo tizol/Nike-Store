18d33ffbe472cf11b8f0ba63ddcc07f99cecdcac
tizol <josephtizol@gmail.com>
2016-01-21 17:08:14 -0800

    first commit

---
+++ collections/products-collection.js
@@ -0,0 +1,14 @@
+Products = new Mongo.Collection("products");
+
+
+// CollectionName.allow({
+//   insert: function(){
+//     return true;
+//   },
+//   update: function(){
+//     return true;
+//   },
+//   remove: function(){
+//     return true;
+//   }
+// });
