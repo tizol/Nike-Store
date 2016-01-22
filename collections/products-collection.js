ad552a8a36108376e544852bf0b123f436fbdce3
tizol <josephtizol@gmail.com>
2016-01-21 17:21:03 -0800

    products collection boilerplate

--- collections/products-collection.js
+++ collections/products-collection.js
@@ -1,14 +1,1 @@
-Products = new Mongo.Collection("products");
 
-
-// CollectionName.allow({
-//   insert: function(){
-//     return true;
-//   },
-//   update: function(){
-//     return true;
-//   },
-//   remove: function(){
-//     return true;
-//   }
-// });
